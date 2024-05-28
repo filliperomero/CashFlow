9b1b51ab6803de6b57943c2af2b2d260d50071d6
filliperomero <fillipe.romero@gmail.com>
2024-05-28 16:07:44 -0600

    Add Get All Expenses Route

---
+++ src/CashFlow.Application/UseCases/Expenses/GetAll/GetAllExpenseUseCase.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CashFlow.Communication.Responses;
+using CashFlow.Domain.Repositories.Expenses;
+
+namespace CashFlow.Application.UseCases.Expenses.GetAll;
+
+internal class GetAllExpenseUseCase : IGetAllExpenseUseCase
+{
+    private readonly IExpensesRepository _expensesRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllExpenseUseCase(IExpensesRepository repository, IMapper mapper)
+    {
+        _expensesRepository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<ResponseExpensesJson> Execute()
+    {
+        var result = await _expensesRepository.GetAll();
+
+        return new ResponseExpensesJson
+        {
+            Expenses = _mapper.Map<List<ResponseShortExpenseJson>>(result)
+        };
+    }
+}
--- src/CashFlow.Communication/Responses/ResponseRegisterExpenseJson.cs
+++ src/CashFlow.Communication/Responses/ResponseRegisterExpenseJson.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CashFlow.Communication.Responses;
 
-namespace CashFlow.Communication.Responses;
 public class ResponseRegisterExpenseJson
 {
     public string Title { get; set; } = string.Empty;

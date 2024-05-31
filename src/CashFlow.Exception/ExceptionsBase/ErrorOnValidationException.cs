e90f705b2ca17021318adae4a885f8494d6e0c47
filliperomero <fillipe.romero@gmail.com>
2024-05-30 18:36:49 -0600

    Refactor Exception Filter to be more generic and refactor exceptions to have status code and GetError()

--- src/CashFlow.Exception/ExceptionsBase/ErrorOnValidationException.cs
+++ src/CashFlow.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -1,11 +1,20 @@
-﻿namespace CashFlow.Exception.ExceptionsBase;
+﻿using System.Net;
+
+namespace CashFlow.Exception.ExceptionsBase;
 
 public class ErrorOnValidationException : CashFlowException
 {
-    public List<string> Errors { get; set; }
+    private List<string> Errors { get; set; }
+
+    public override int StatusCode => (int)HttpStatusCode.BadRequest;
 
     public ErrorOnValidationException(List<string> errorMessages) : base(string.Empty)
     {
         Errors = errorMessages;
+    }
+
+    public override List<string> GetErrors()
+    {
+        return Errors;
     }
 }

5eaa7cdafbb091ead069a7106fe25839d69a2ea3
filliperomero <fillipe.romero@gmail.com>
2024-10-24 23:56:38 -0600

    Add Register User Test

---
+++ tests/WebAPI.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using CashFlow.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Test;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder
+            .UseEnvironment("Test")
+            .ConfigureServices(services =>
+            {
+                var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+                
+                services.AddDbContext<CashFlowDbContext>(config =>
+                {
+                    config.UseInMemoryDatabase("InMemoryDbForTesting");
+                    config.UseInternalServiceProvider(provider);
+                });
+            });
+    }
+}
---
+++ tests/WebAPI.Tests/Users/RegisterUserTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace WebAPI.Test.Users;
+
+public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
+{
+    private const string USER_URI = "api/User";
+    
+    private readonly HttpClient _httpClient;
+
+    public RegisterUserTest(CustomWebApplicationFactory webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var result = await _httpClient.PostAsJsonAsync(USER_URI, request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.Created);
+        
+        var body = await result.Content.ReadAsStreamAsync();
+        
+        var response = await JsonDocument.ParseAsync(body);
+
+        response.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
+        response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
+    }
+}

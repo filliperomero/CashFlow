856d2ed3c49fdd7241392841df0ea9f597f691b1
Fillipe Romero <fromero@jetnet.com>
2024-10-28 17:54:11 -0600

    Add error test for empty name in Register User Test

--- tests/WebAPI.Tests/Users/RegisterUserTest.cs
+++ tests/WebAPI.Tests/Users/RegisterUserTest.cs
@@ -1,8 +1,10 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
+using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
+using Irony.Parsing;
 
 namespace WebAPI.Test.Users;
 
@@ -32,5 +34,24 @@ public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 
         response.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
         response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
+    }
+    
+    [Fact]
+    public async Task Error_Empty_Name()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var result = await _httpClient.PostAsJsonAsync(USER_URI, request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        
+        var body = await result.Content.ReadAsStreamAsync();
+        
+        var response = await JsonDocument.ParseAsync(body);
+
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(ResourceErrorMessages.NAME_EMPTY));
     }
 }

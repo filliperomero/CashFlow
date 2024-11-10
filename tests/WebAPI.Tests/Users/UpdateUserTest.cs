84a20cc225c315535c5fdcc689276a46c526e067
Fillipe Romero <fillipe.romero@gmail.com>
2024-11-10 12:07:03 -0700

    Add Update User Tests (Use Case & API)

---
+++ tests/WebAPI.Tests/Users/UpdateUserTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using WebAPI.Test.InlineData;
+
+namespace WebAPI.Test.Users;
+
+public class UpdateUserTest : CashFlowClassFixture
+{
+    private const string USER_URI = "api/User";
+    
+    private readonly string _token;
+
+    public UpdateUserTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory.User_Team_Member.GetToken();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestUpdateUserJsonBuilder.Build();
+        var result = await PutAsync(requestUri: USER_URI, token: _token, request: request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+    
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Empty_Name(string cultureInfo)
+    {
+        var request = RequestUpdateUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var result = await PutAsync(requestUri: USER_URI, request: request, token: _token, culture: cultureInfo);
+
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        
+        var body = await result.Content.ReadAsStreamAsync();
+        
+        var response = await JsonDocument.ParseAsync(body);
+
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(cultureInfo));
+
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
+    }
+}

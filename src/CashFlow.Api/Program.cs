949b0cfaa0e7547f572c6f56e141b5cd0e93c658
filliperomero <fillipe.romero@gmail.com>
2024-11-12 23:02:02 -0700

    Add Health Check

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -5,9 +5,12 @@ using CashFlow.Api.Token;
 using CashFlow.Application;
 using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure;
+using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.Extensions;
 using CashFlow.Infrastructure.Migrations;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
@@ -76,7 +79,21 @@ builder.Services.AddAuthentication(config =>
     };
 });
 
+// Add HealthChecks to builder
+builder.Services.AddHealthChecks().AddDbContextCheck<CashFlowDbContext>();
+
 var app = builder.Build();
+
+// Configure Health Check Endpoint
+app.MapHealthChecks("/Health", new HealthCheckOptions
+{
+    AllowCachingResponses = false,
+    ResultStatusCodes =
+    {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+    }
+});
 
 if (app.Environment.IsDevelopment())
 {

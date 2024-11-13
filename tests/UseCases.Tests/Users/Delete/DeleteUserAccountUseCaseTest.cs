ba3b4b3947665a30a50d9737b583e7cae16fedb4
filliperomero <fillipe.romero@gmail.com>
2024-11-12 18:38:23 -0700

    Add Delete User Account Tests (Use Case & API)

---
+++ tests/UseCases.Tests/Users/Delete/DeleteUserAccountUseCaseTest.cs
@@ -0,0 +1,31 @@
+﻿using CashFlow.Application.UseCases.Users.Delete;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories;
+using FluentAssertions;
+
+namespace UseCases.Tests.Users.Delete;
+
+public class DeleteUserAccountUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Builder();
+        var useCase = CreateUseCase(user);
+
+        var act = async () => await useCase.Execute();
+
+        await act.Should().NotThrowAsync();
+    }
+
+    private DeleteUserAccountUseCase CreateUseCase(User user)
+    {
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var repository = UserWriteOnlyRepositoryBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new DeleteUserAccountUseCase(loggedUser, repository, unitOfWork);
+    }
+}
---
+++ tests/WebAPI.Tests/Users/DeleteUserAccountTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using FluentAssertions;
+
+namespace WebAPI.Test.Users;
+
+public class DeleteUserAccountTest : CashFlowClassFixture
+{
+    private const string USER_URI = "api/User";
+    
+    private readonly string _token;
+
+    public DeleteUserAccountTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory.User_Team_Member.GetToken();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var result = await DeleteAsync(USER_URI, _token);
+
+        result.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+}

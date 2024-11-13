111dd1d0faf7b53812dcb358cdf05979844050ff
filliperomero <fillipe.romero@gmail.com>
2024-11-12 21:27:58 -0700

    Add association between expense and tags

--- src/CashFlow.Application/UseCases/Expenses/ExpenseValidator.cs
+++ src/CashFlow.Application/UseCases/Expenses/ExpenseValidator.cs
@@ -11,5 +11,9 @@ public class ExpenseValidator : AbstractValidator<RequestExpenseJson>
         RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
         RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.EXPENSE_CANNOT_HAVE_FUTURE_DATE);
         RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
+        RuleFor(expense => expense.Tags).ForEach(rule =>
+        {
+            rule.IsInEnum().WithMessage(ResourceErrorMessages.TAG_TYPE_NOT_SUPPORTED);
+        });
     }
 }
